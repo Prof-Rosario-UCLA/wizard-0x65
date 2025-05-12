435fdf0e4f6f860ff1ef15b988aa8aa0203a2d6e
Aubrey Clark <zane.a.s.clark@gmail.com>
2025-05-12 15:39:08 -0700

    set up to work with google app engine, and started setting up github actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+	distDir: "build", // required for Google App Engine
 	webpack: (config) => {
 		config.experiments.asyncWebAssembly = true;
 		return config;
