cca5215c8b125e6fc5fe3278bd5e285499c63cab
Aubrey Clark <zane.a.s.clark@gmail.com>
2025-05-12 15:47:49 -0700

    removed gitignore from renderer

---
+++ app/renderer/renderer_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const greet: () => void;
+export const main: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure1358_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1366_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure1360_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1352_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1364_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1356_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__haba832465072f121: (a: number, b: number) => void;
+export const closure1362_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1354_externref_shim: (a: number, b: number, c: any) => void;
+export const closure41821_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
