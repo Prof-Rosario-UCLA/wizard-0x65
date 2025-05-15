0118a79a03342d592d2d3647c702fb633fe495d7
Aubrey Clark <zane.a.s.clark@gmail.com>
2025-05-14 17:41:02 -0700

    added events to simulation

--- src/simulation/simulation.test.ts
+++ src/simulation/simulation.test.ts
@@ -1,7 +1,212 @@
-import { Game } from "./simulation";
+import { CardBase, CardDiedEvent, Game, GameEvent, GameEventType, GameState, HealthChangedEvent, RoundStartEvent as RoundStartedEvent, WinState } from "./simulation";
 
-describe("meow", () => {
-	test("basic", () => {
-		expect(true).toBe(true);
+class TestCard extends CardBase {
+	constructor(deckCardId: string, health: number, damage: number) {
+		super(deckCardId, health, damage);
+	}
+
+	get id(): string {
+		return "test";
+	}
+
+	get name(): string {
+		return "Test Card";
+	}
+}
+
+class EventLoggerCard extends TestCard {
+	startedEvents: RoundStartedEvent[] = []
+	cardHealthChangedEvents: HealthChangedEvent[] = []
+	cardDiedEvents: CardDiedEvent[] = []
+
+	constructor(deckCardId: string, health: number, damage: number) {
+		super(deckCardId, health, damage);
+	}
+
+	onEvent(event: GameEvent): void {
+		if (event instanceof RoundStartedEvent) {
+			this.startedEvents.push(event as RoundStartedEvent);
+		}
+		if (event instanceof HealthChangedEvent) {
+			this.cardHealthChangedEvents.push(event as HealthChangedEvent);
+		}
+		if (event instanceof CardDiedEvent) {
+			this.cardDiedEvents.push(event as CardDiedEvent);
+		}
+	}
+};
+
+class FriendlyDamageHalverCard extends TestCard {
+	constructor(deckCardId: string, health: number, damage: number) {
+		super(deckCardId, health, damage);
+	}
+
+	onEvent(event: GameEvent): void {
+		if (event instanceof HealthChangedEvent) {
+			if(event.card.team !== this.team || event.delta > 0) {
+				return;
+			}
+
+			event.delta = Math.floor(event.delta / 2);
+		}
+	}
+}
+
+class ReviveSelfCard extends TestCard {
+	#revived = false;
+
+	constructor(deckCardId: string, health: number, damage: number) {
+		super(deckCardId, health, damage);
+	}
+
+	onEvent(event: GameEvent): void {
+		if(this.#revived) return;
+		if (event instanceof CardDiedEvent) {
+			if(event.card.id !== this.id) {
+				return;
+			}
+
+			this.health = 1;
+			event.cancelled = true;
+
+			this.#revived = true;
+		}
+	}
+}
+
+describe("Simulation", () => {
+	describe("Starting with no cards", () => {
+		test("Enemy loss", () => {
+			const playerDeck = [];
+			const enemyDeck = [new TestCard("", 1, 1)];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Over);
+			expect(game.winState).toBe(WinState.Enemy);
+
+			expect(game.step()).toBe(GameState.Over);
+		});
+
+		test("Enemy loss", () => {
+			const playerDeck = [new TestCard("", 1, 1)];
+			const enemyDeck = [];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Over);
+			expect(game.winState).toBe(WinState.Player);
+
+			expect(game.step()).toBe(GameState.Over);
+		});
+
+		test("Stalemate", () => {
+			const playerDeck = [];
+			const enemyDeck = [];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Over);
+			expect(game.winState).toBe(WinState.Stalemate);
+
+			expect(game.step()).toBe(GameState.Over);
+		});
+	});
+
+	describe("Attacks", () => {
+		test("Player attacks enemy", () => {
+			const playerDeck = [new TestCard("", 4, 2)];
+			const enemyDeck = [new TestCard("", 5, 2)];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Running);
+
+			// turn 1: 4 -> 2, 5 -> 3
+			expect(game.step()).toBe(GameState.Running);
+
+			expect(game.playerDeck.length).toBe(1);
+			expect(game.playerDeck[0].health).toBe(2);
+
+			expect(game.enemyDeck.length).toBe(1);
+			expect(game.enemyDeck[0].health).toBe(3);
+
+			// turn 2: 1 -> dead, 3 -> 1
+			expect(game.step()).toBe(GameState.Over);
+			expect(game.playerDeck.length).toBe(0);
+			expect(game.enemyDeck.length).toBe(1);
+			expect(game.enemyDeck[0].health).toBe(1);
+			expect(game.winState).toBe(WinState.Enemy);
+		});
+	});
+
+	describe("Events", () => {
+		test("Round started, health changed, card died", () => {
+			const playerCard = new EventLoggerCard("", 4, 2);
+			const playerDeck = [playerCard];
+			const enemyDeck = [new TestCard("", 5, 3)];
+
+			const startedEvents = playerCard.startedEvents;
+			const cardHealthChangedEvents = playerCard.cardHealthChangedEvents;
+			const cardDiedEvents = playerCard.cardDiedEvents;
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Running);
+			expect(startedEvents.length).toBe(0);
+
+			expect(game.step()).toBe(GameState.Running);
+			expect(startedEvents.length).toBe(1);
+			expect(cardHealthChangedEvents.length).toBe(2);
+			expect(cardDiedEvents.length).toBe(0);
+
+			expect(cardHealthChangedEvents[0].card).toBe(enemyDeck[0]);
+			expect(cardHealthChangedEvents[0].delta).toBe(-2);
+			expect(cardHealthChangedEvents[1].card).toBe(playerDeck[0]);
+			expect(cardHealthChangedEvents[1].delta).toBe(-3);
+
+			startedEvents.length = 0;
+			cardHealthChangedEvents.length = 0;
+			cardDiedEvents.length = 0;
+
+			expect(game.step()).toBe(GameState.Over);
+			expect(startedEvents.length).toBe(0);
+			expect(cardHealthChangedEvents.length).toBe(2);
+			expect(cardDiedEvents.length).toBe(1);
+
+			expect(cardHealthChangedEvents[0].card).toBe(enemyDeck[0]);
+			expect(cardHealthChangedEvents[1].card).toBe(playerCard);
+
+			expect(cardDiedEvents[0].card).toBe(playerCard);
+		});
+	});
+
+	describe("Modifying events", () => {
+		test("Modifying health changed event", () => {
+			const playerDeck = [new TestCard("", 4, 2)];
+			const enemyDeck = [new FriendlyDamageHalverCard("", 5, 3)];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.gameState).toBe(GameState.Running);
+
+			expect(game.step()).toBe(GameState.Running);
+			expect(game.playerDeck.length).toBe(1);
+			expect(game.enemyDeck[0].health).toBe(4);
+		});
+
+		test("Revive card", () => {
+			const playerDeck = [new ReviveSelfCard("", 1, 1)];
+			const enemyDeck = [new TestCard("", 3, 1)];
+
+			const game = new Game(playerDeck, enemyDeck);
+
+			expect(game.step()).toBe(GameState.Running);
+			expect(game.playerDeck.length).toBe(1);
+			expect(game.playerDeck[0].health).toBe(1);
+
+			expect(game.step()).toBe(GameState.Over);
+			expect(game.playerDeck.length).toBe(0);
+		});
 	});
 });
--- src/simulation/simulation.ts
+++ src/simulation/simulation.ts
@@ -1,25 +1,147 @@
+export enum CardState {
+	Alive,
+	Dead,
+}
+
+export enum CardTeam {
+	Player,
+	Enemy,
+}
+
 export class CardBase {
-	public id: string;
+	/* These are part of the card's identity */
+	/**
+	 * The database ID of the card.
+	 */
+	public deckCardId: string;
+
+	/**
+	 * The current health of the card.
+	 */
 	public health: number;
+
+	/**
+	 * The base damage of the card.
+	 */
 	public damage: number;
 
-	constructor(id: string) {
-		this.id = id;
+	/* These are part of the runtime state */
+	public state: CardState = CardState.Alive;
+	public team: CardTeam = CardTeam.Player;
+	public game?: Game;
+
+	constructor(deckCardId: string, health: number, damage: number) {
+		this.deckCardId = deckCardId;
+		this.health = health;
+		this.damage = damage;
 	}
 
+	/**
+	 * The ID of the card. This is used in API requests and the database.
+	 */
+	get id(): string {
+		return "base";
+	}
+
+	/**
+	 * The name of the card. This is used in the UI.
+	 * This should be a human-readable name.
+	 */
 	get name(): string {
-		return null;
+		return "Base Card";
 	}
 
-	bindEffects(): void {}
+	/**
+	 * Attacks the enemy card.
+	 * @param enemyCard The card to attack.
+	 */
+	attack(enemyCard: CardBase): void {
+		enemyCard.takeDamage(this.damage);
+	}
+
+	/**
+	 * Takes damage from an attack.
+	 * @param amount The amount of damage to deal.
+	 */
+	takeDamage(amount: number): void {
+		const event = new HealthChangedEvent();
+		event.card = this;
+		event.delta = -amount;
+		this.game.broadcast(event);
+
+		if(event.cancelled) {
+			return;
+		}
+
+		this.health += event.delta;
+		if (this.health <= 0) {
+			const event = new CardDiedEvent();
+			event.card = this;
+			this.game.broadcast(event);
+
+			// if some card healed us, don't die
+			if(this.health <= 0) {
+				this.state = CardState.Dead;
+			}
+		}
+	}
+
+	onEvent(event: GameEvent): void {}
 };
 
-enum GameState {
+export enum GameEventType {
+	RoundStart,
+
+	/**
+	 * Fired when the round ends.
+	 * Note that this only occurs on the server, since
+	 * the client doesn't simulate during the shop.
+	 */
+	RoundEnd,
+	HealthChanged,
+	CardDied,
+};
+
+export class GameEvent {
+	cancelled: boolean = false;
+	get type(): GameEventType {
+		return null
+	}
+};
+
+/**
+ * Fired when the round is started. Note that
+ * this is called on the first *step*, not on construction.
+ */
+export class RoundStartEvent extends GameEvent {
+	get type(): GameEventType {
+		return GameEventType.RoundStart;
+	}
+};
+
+export class HealthChangedEvent extends GameEvent {
+	public card: CardBase;
+	public delta: number;
+
+	get type(): GameEventType {
+		return GameEventType.HealthChanged;
+	}
+};
+
+export class CardDiedEvent extends GameEvent {
+	public card: CardBase;
+
+	get type(): GameEventType {
+		return GameEventType.CardDied;
+	}
+};
+
+export enum GameState {
 	Running,
 	Over,
 };
 
-enum WinState {
+export enum WinState {
 	Player,
 	Enemy,
 	Stalemate,
@@ -32,12 +154,53 @@ export class Game {
 	public gameState: GameState = GameState.Running;
 	public winState?: WinState = null;
 
+	#started: boolean = false;
+
+	constructor(playerDeck: CardBase[], enemyDeck: CardBase[]) {
+		this.playerDeck = playerDeck;
+		this.enemyDeck = enemyDeck;
+
+		this.#forEachCard((card, team) => {
+			card.game = this;
+			card.team = team;
+		});
+
+		// update win state immediately
+		// for the edge cases of the player/enemy
+		// having no cards at the start of the game
+		this.#updateWinState();
+	}
+
 	step(): GameState {
-		// if already over, return
+		// update win state and return early if game is already over
+		// note: this is somewhat redundant, but it makes the code easier to read
 		if (this.gameState === GameState.Over) {
 			return this.gameState;
 		}
 
+		if (!this.#started) {
+			this.#started = true;
+			const event = new RoundStartEvent();
+			this.broadcast(event);
+		}
+
+		const playerCard = this.playerDeck[0];
+		const enemyCard = this.enemyDeck[0];
+
+		// attack cards
+		playerCard.attack(enemyCard);
+		enemyCard.attack(playerCard);
+
+		// remove all dead cards
+		this.playerDeck = this.playerDeck.filter((card) => card.state !== CardState.Dead);
+		this.enemyDeck = this.enemyDeck.filter((card) => card.state !== CardState.Dead);
+
+		// update win state again
+		this.#updateWinState();
+		return this.gameState;
+	}
+
+	#updateWinState(): void {
 		if (this.playerDeck.length === 0 && this.enemyDeck.length === 0) {
 			this.gameState = GameState.Over;
 			this.winState = WinState.Stalemate;
@@ -50,15 +213,16 @@ export class Game {
 			this.gameState = GameState.Over;
 			this.winState = WinState.Player;
 		}
+	}
 
-		// if just ended, return
-		if (this.gameState === GameState.Over) {
-			return this.gameState;
-		}
+	#forEachCard(
+		callback: (card: CardBase, cardTeam: CardTeam) => void
+	): void {
+		this.playerDeck.forEach(card => callback(card, CardTeam.Player));
+		this.enemyDeck.forEach(card => callback(card, CardTeam.Enemy));
+	}
 
-		const playerCard = this.playerDeck[0];
-		const enemyCard = this.enemyDeck[0];
-
-		return this.gameState;
+	broadcast(event: GameEvent): void {
+		this.#forEachCard((card) => card.onEvent(event));
 	}
 };
