dfa1e1d3074b164f8a6a42c2b3138e64cc1de528
Rachel Jin <racheljin24@gmail.com>
2025-05-25 15:20:01 -0700

    new simulation component

---
+++ src/app/components/simulation.tsx
@@ -0,0 +1,23 @@
+// function Simulation() {
+//     return (
+//         <>
+//             <div className="flex">
+//                 <div className="border rounded-md px-4 py-2"></div>
+//             </div>
+//         </>
+//     );
+// }
+
+// export { Simulation };
+
+export type SimulationProps = {};
+
+export function Simulation(props: SimulationProps) {
+    const {} = props;
+
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import { Button } from "./components/button";
 import { Card } from "./components/card";
+import { Simulation } from "./components/simulation";
 
 export default function Home() {
     return (
         <main>
             <h1>Wizard 0x65</h1>
-
+            <Simulation />
+            {/* 
             <Link href="/shop">
                 <Button>Hello</Button>
             </Link>
-            <Card name="ocaml" scale={2} health={5} dmg={2} />
+            <Card name="ocaml" scale={2} health={5} dmg={2} /> */}
         </main>
     );
 }
