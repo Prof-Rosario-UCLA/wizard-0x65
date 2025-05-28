4af610d9d2bb9e0946b1e36f3a0b9ebcd37f01cc
Rachel Jin <racheljin24@gmail.com>
2025-05-28 12:41:25 -0700

    metadata and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,34 @@
+import { Metadata } from "next";
 import "./globals.css";
 
+export const metadata: Metadata = {
+    title: "Wizard 0x65",
+    description: "A software-themed autobattler!",
+};
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" className="">
+            <link
+                rel="icon"
+                type="image/png"
+                href="/favicon-96x96.png"
+                sizes="96x96"
+            />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+
+            <body>{children}</body>
+        </html>
+    );
 }
